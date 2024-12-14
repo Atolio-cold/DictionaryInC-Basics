fdf85c0755af02b59fd88208f4d599258d8d53e9
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-14 22:02:05 +0100

    Add files via upload

---
+++ BasicCalculatorCurrency/Currency.cs
@@ -0,0 +1,17 @@
+
+
+namespace BasicCalculatorCurrency
+{
+    public class Currency
+    {
+        public Currency(string name, string fullName, double rate)
+        {
+            Name = name;
+            FullName = fullName;
+            Rate = rate;
+        }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public double Rate { get; set; }
+    }
+}
